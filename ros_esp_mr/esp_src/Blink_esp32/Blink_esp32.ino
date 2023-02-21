0dc4362ca9bb510a312eeb3b042c94cc65fdf0a5
IvaMurataya <mura.ivan@hotmail.com>
2023-02-21 15:09:41 -0600

    Trtle sim controlled by an ultrasonic sensor

--- ros_esp_mr/esp_src/Blink_esp32/Blink_esp32.ino
+++ ros_esp_mr/esp_src/Blink_esp32/Blink_esp32.ino
@@ -26,10 +26,12 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(2, OUTPUT);
+  Serial.begin(57600);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  Serial.write("Hola");
   digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
---
+++ ros_esp_mr/src/ultrasonic_turtlesim.py
@@ -0,0 +1,22 @@
+import rospy
+from std_msgs.msg import Int16
+from geometry_msgs.msg import Twist
+
+def callback(data):
+    global pub
+    tbsim_vel = Twist()
+    ultrasonic_value = data.data
+    if (ultrasonic_value < 20):
+        tbsim_vel.angular.z = 0.4
+    else:
+        tbsim_vel.linear.x = 0.6
+    pub.publish(tbsim_vel)
+    
+def listener():
+    global pub 
+    rospy.init_node('ultrasonic_tbsim_driver_node', anonymous=True )
+    pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
+    rospy.Subscriber("ultrasonic_values", Int16, callback)
+    rospy.spin()
+if __name__ == '__main__':
+    listener()
